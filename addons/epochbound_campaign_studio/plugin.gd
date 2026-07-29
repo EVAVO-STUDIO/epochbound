54868f96cbab3680f418254b9ad43b360dd064e7
EVAVO-STUDIO <evavo.studio@gmail.com>
2026-07-29 13:51:01 +1000

    fix: load guarded world-builder controller

--- addons/epochbound_campaign_studio/plugin.gd
+++ addons/epochbound_campaign_studio/plugin.gd
@@ -1,7 +1,7 @@
 @tool
 extends EditorPlugin
 
-const CampaignStudio = preload("res://addons/epochbound_campaign_studio/campaign_studio.gd")
+const CampaignStudio = preload("res://addons/epochbound_campaign_studio/world_builder_studio.gd")
 
 var studio
 

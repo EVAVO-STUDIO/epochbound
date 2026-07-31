296f185aa1712db3b1b09eca3c62eb63cb7b84bd
EVAVO-STUDIO <evavo.studio@gmail.com>
2026-08-01 03:14:43 +1000

    fix: enable animation-aware campaign scaffolding

--- addons/epochbound_campaign_studio/plugin.gd
+++ addons/epochbound_campaign_studio/plugin.gd
@@ -1,9 +1,10 @@
 @tool
 extends EditorPlugin
 
-const CampaignStudio = preload("res://addons/epochbound_campaign_studio/campaign_studio_current.gd")
+const CampaignStudio = preload("res://addons/epochbound_campaign_studio/campaign_studio_animation_current.gd")
 
 var studio
+
 
 func _enter_tree() -> void:
 	studio = CampaignStudio.new()
@@ -11,20 +12,25 @@ func _enter_tree() -> void:
 	studio.visible = false
 	EditorInterface.get_editor_main_screen().add_child(studio)
 
+
 func _exit_tree() -> void:
 	if is_instance_valid(studio):
 		studio.queue_free()
 	studio = null
 
+
 func _has_main_screen() -> bool:
 	return true
+
 
 func _make_visible(visible: bool) -> void:
 	if is_instance_valid(studio):
 		studio.visible = visible
 
+
 func _get_plugin_name() -> String:
 	return "Campaign"
+
 
 func _get_plugin_icon() -> Texture2D:
 	return EditorInterface.get_editor_theme().get_icon("Node", "EditorIcons")
